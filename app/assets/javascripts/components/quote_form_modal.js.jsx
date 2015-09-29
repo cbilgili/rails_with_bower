71415ac35d530b032fce68f6dcd31fc25acef10d
Canbey Bilgili <cbilgili@gmail.com>
2015-09-29 17:45:40 +0300

    Modal update

--- app/assets/javascripts/components/quote_form_modal.js.jsx
+++ app/assets/javascripts/components/quote_form_modal.js.jsx
@@ -1,19 +1,19 @@
-var Modal = require('react-modal');
+var Modal = require('modal');
+
 var QuoteFormModal = React.createClass({
   getInitialState: function() {
     return {
       content: '1',
       url: '2',
       notes: '3',
-      modalIsOpen: false
+      isOpened: false
     };
   },
-  openModal: function() {
-    this.setState({modalIsOpen: true});
+  open: function(){
+    this.setState({isOpened: true});
   },
- 
-  closeModal: function() {
-    this.setState({modalIsOpen: false});
+  close: function(){
+    this.setState({isOpened: false});
   },
   componentDidMount: function() {
     // $('.modal')
@@ -42,26 +42,33 @@ var QuoteFormModal = React.createClass({
   render: function() {
     return (
       <div>
-        <button className='ui button' onClick={this.openModal}>Add New Quote</button>
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onRequestClose={this.closeModal}>
- 
-          <h2>Hello</h2>
-          <button onClick={this.closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
-          </form>
+        <button className='ui button blue' onClick={this.open.bind(this)}>Click me</button>
+        <Modal style='standard' size='fullscreen' isOpened={this.state.isOpened} closeIcon closeOnOutsideClick onClose={this.close.bind(this)}>
+          <div className='header'>
+            Profile Picture
+          </div>
+          <div className='image content'>
+            <div className='ui medium image'>
+              <img src='http://semantic-ui.com/images/avatar2/large/rachel.png' />
+            </div>
+            <div className='description'>
+              <div className='ui header'>We've auto-chosen a profile image for you.</div>
+              <p>We've grabbed the following image from the <a href='https://www.gravatar.com' target='_blank'>gravatar</a> image associated with your registered e-mail address.</p>
+              <p>Is it okay to use this photo?</p>
+            </div>
+          </div>
+          <div className='actions'>
+            <div className='ui black deny button' onClick={this.close.bind(this)}>
+              Nope
+            </div>
+            <div className='ui positive right labeled icon button'>
+              Yep, thats me
+              <i className='checkmark icon'></i>
+            </div>
+          </div>
         </Modal>
-
       </div>
     );
   }
 });
-
 module.exports = QuoteFormModal;
